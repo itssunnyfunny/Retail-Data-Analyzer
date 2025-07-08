9093fc65009a5256ce78bd3cd38df52ecc63e3f3
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-07-08 10:34:27 +0530

    fixing the express5 issue

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,29 +1,35 @@
-import { Request, Response } from 'express';
+import { Request, Response, RequestHandler } from 'express';
 import { productService } from '../services/productService';
 
-export const getAllProducts = async (req: Request, res: Response) => {
+export const getAllProducts: RequestHandler = async (req, res) => {
   const products = await productService.getAll();
   res.json(products);
 };
 
-export const createProduct = async (req: Request, res: Response) => {
+export const createProduct: RequestHandler = async (req, res) => {
   const product = await productService.create(req.body);
   res.status(201).json(product);
 };
 
-export const getProductById = async (req: Request, res: Response) => {
+export const getProductById: RequestHandler = async (req, res) => {
   const product = await productService.getById(Number(req.params.id));
-  if (!product) return res.status(404).json({ message: 'Product not found' });
+  if (!product) {
+    res.status(404).json({ message: 'Product not found' });
+    return;
+  }
   res.json(product);
 };
 
-export const updateProduct = async (req: Request, res: Response) => {
+export const updateProduct: RequestHandler = async (req, res) => {
   const product = await productService.update(Number(req.params.id), req.body);
-  if (!product) return res.status(404).json({ message: 'Product not found' });
+  if (!product) {
+    res.status(404).json({ message: 'Product not found' });
+    return;
+  }
   res.json(product);
 };
 
-export const deleteProduct = async (req: Request, res: Response) => {
+export const deleteProduct: RequestHandler = async (req, res) => {
   await productService.delete(Number(req.params.id));
   res.status(204).send();
 }; 
